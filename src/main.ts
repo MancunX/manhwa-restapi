748029d4049bfc8448532a28566bc2a241e86c70
MancunX <mhdimam00@gmail.com>
2024-04-24 12:03:25 +0700

    feat: Added Swagger UI integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const appOptions = {
@@ -7,6 +8,13 @@ async function bootstrap() {
   };
   const app = await NestFactory.create(AppModule, appOptions);
   app.enableShutdownHooks();
+  const swaggerConf = new DocumentBuilder()
+    .setTitle('Manhwa RESTapi')
+    .setVersion('0.0.1')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConf);
+  SwaggerModule.setup('api', app, swaggerDocument);
   await app.listen(3000);
 }
 bootstrap();
