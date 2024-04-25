d84676904d8734a9eb197042727f33b1649c0b75
MancunX <mhdimam00@gmail.com>
2024-04-25 12:05:36 +0700

    feat: Include UsersModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ValidationModule } from './validation/validation.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, ValidationModule],
+  imports: [PrismaModule, ValidationModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
