baf60119012235393b7e96a6f3b8de4f97358100
MancunX <mhdimam00@gmail.com>
2024-04-25 11:58:13 +0700

    Update AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ValidationModule } from './validation/validation.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ValidationModule],
   controllers: [AppController],
   providers: [AppService],
 })
