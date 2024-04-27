dd25c1b08e4543471e8109d5ddb9313be5833dbd
MancunX <mhdimam00@gmail.com>
2024-04-27 17:45:17 +0700

    Added classes for authentication-related responses and requests

---
+++ src/model/auth.model.ts
@@ -0,0 +1,13 @@
+export class SignInResponse {
+  access_token: string;
+  refresh_token: string;
+}
+
+export class SignInRequest {
+  username: string;
+  password: string;
+}
+
+export class RefreshTokenResponse {
+  access_token: string;
+}
