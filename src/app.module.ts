4d8c83ef27c1bf7f36086ad16fcb81867301ea28
MancunX <mhdimam00@gmail.com>
2024-05-08 13:35:56 +0700

    Added modules for comics, genres, and comic types to the main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,19 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ValidationModule } from './validation/validation.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-
+import { ComicsModule } from './comics/comics.module';
+import { GenresModule } from './genres/genres.module';
+import { ComicTypesModule } from './comic-types/comic-types.module';
 @Module({
-  imports: [PrismaModule, ValidationModule, UsersModule, AuthModule],
+  imports: [
+    PrismaModule,
+    ValidationModule,
+    UsersModule,
+    AuthModule,
+    ComicsModule,
+    GenresModule,
+    ComicTypesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
