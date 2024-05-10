84687b5d3b1138aa756a32b119e7a71582fc7f82
MancunX <mhdimam00@gmail.com>
2024-05-10 14:44:25 +0700

    Updated Users module to remove the JwtAuthGuard and RolesGuard for authentication

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,25 +1,12 @@
-import { ValidationModule } from './../validation/validation.module';
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
-import { RolesGuard } from 'src/auth/guard/role.guard';
+import { ValidationModule } from 'src/validation/validation.module';
 
 @Module({
   imports: [ValidationModule],
-  providers: [
-    UsersService,
-    {
-      provide: APP_GUARD,
-      useClass: JwtAuthGuard,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: RolesGuard,
-    },
-  ],
   controllers: [UsersController],
+  providers: [UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}
