bc7439d867e5aba67e675b24a09757419dabe0df
MancunX <mhdimam00@gmail.com>
2024-05-13 15:39:24 +0700

    Updated the JWT constants:
    - Changed the structure of the constants object to use environment variables for the secret keys.
    - Renamed the constants to `ACCESS_SECRET_KEY` and `REFRESH_SECRET_KEY` to reflect their purpose more clearly.

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,6 +1,4 @@
 export const jwtConstants = {
-  accessSecret:
-    '17e0899b0d8b22efdfac3049cbe42cc17e959f419462892feddb793790e20fca',
-  refreshSecret:
-    '46ffba0581b1d17c6246ac48f38780c4bb5955d5f342f909fe18552d90d74a17',
+  ACCESS_SECRET_KEY: process.env.ACCESS_SECRET_KEY,
+  REFRESH_SECRET_KEY: process.env.REFRESH_SECRET_KEY,
 };
