05147527035c052ed7069b3b65992500684dc50c
MancunX <mhdimam00@gmail.com>
2024-05-13 15:49:35 +0700

    The AuthController has been refactored to include methods for signing in, signing out, refreshing tokens, and changing passwords. The profile method now retrieves the user profile using the access token from the request cookies. The changePassword method allows users with the 'super' or 'admin' role to change their passwords. Proper error handling and validation have been implemented throughout the controller.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,40 +9,62 @@ import {
   Request as NestReq,
   Res,
   UnauthorizedException,
+  HttpStatus,
+  Patch,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import {
+  AuthChangePasswordRequest,
   AuthProfileResponse,
-  RefreshTokenResponse,
   SignInRequest,
   SignInResponse,
 } from 'src/model/auth.model';
 import { WebResponse } from 'src/model/web.model';
 import { Public } from 'src/auth/decorator/public.decorator';
 import { Response, Request } from 'express';
-import { JwtService } from '@nestjs/jwt';
-import { UsersService } from 'src/users/users.service';
 import { ApiTags } from '@nestjs/swagger';
+import { Roles } from './decorator/role.decorator';
 
 @Controller('api/auth')
 @ApiTags('Authentication')
 export class AuthController {
-  constructor(
-    private authService: AuthService,
-    private jwtService: JwtService,
-    private usersService: UsersService,
-  ) {}
+  constructor(private authService: AuthService) {}
 
   @Get('profile')
   @HttpCode(200)
-  async getProfile(@NestReq() req): Promise<WebResponse<AuthProfileResponse>> {
-    const user = req.user;
+  async profile(
+    @NestReq() req: Request,
+  ): Promise<WebResponse<AuthProfileResponse>> {
+    const token = req.cookies['access_token'];
+    if (!token) {
+      throw new HttpException(
+        'Access token is missing',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    const profile = await this.authService.profile(token);
     return {
-      data: {
-        id: user.id,
-        username: user.username,
-        role: user.role,
-      },
+      data: profile,
+    };
+  }
+
+  @Roles('super', 'admin')
+  @Patch('profile/change-password')
+  @HttpCode(201)
+  async changePassword(
+    @Req() req: Request,
+    @Body() changePassword: AuthChangePasswordRequest,
+  ): Promise<any> {
+    const accessToken = req.cookies['access_token'];
+    if (!accessToken) {
+      throw new HttpException(
+        'Access token is missing',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    await this.authService.changePassword(accessToken, changePassword);
+    return {
+      message: 'Password changed successfully',
     };
   }
 
@@ -53,72 +75,57 @@ export class AuthController {
     @Body() request: SignInRequest,
     @Res({ passthrough: true }) res: Response,
   ): Promise<WebResponse<SignInResponse>> {
-    const { accessToken, refreshToken } =
-      await this.authService.signIn(request);
+    const { accessToken } = await this.authService.signIn(request);
     res.cookie('access_token', accessToken, {
       httpOnly: true,
       secure: false,
       sameSite: 'none',
       maxAge: 60 * 1000,
     });
-    res.cookie('refresh_token', refreshToken, {
-      httpOnly: true,
-      secure: false,
-      sameSite: 'none',
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
     return {
       message: 'Login success',
     };
   }
 
-  @Public()
+  @Roles('super', 'admin')
   @Post('signOut')
   @HttpCode(200)
   async signOut(
     @Res({ passthrough: true }) res: Response,
     @Req() req: Request,
+    @Body('refreshToken') refreshToken: string,
   ): Promise<any> {
+    await this.authService.signOut(refreshToken);
     const accessToken = req.cookies['access_token'];
-    const refreshToken = req.cookies['refresh_token'];
-
-    if (!accessToken && !refreshToken) {
+    if (!accessToken) {
       throw new UnauthorizedException();
     }
     res.clearCookie('access_token');
-    res.clearCookie('refresh_token');
     return {
       message: 'Logout success',
     };
   }
 
-  @Public()
-  @Get('token')
+  @Roles('super', 'admin')
+  @Post('refresh-token')
   @HttpCode(201)
   async refreshToken(
-    @Req() req: Request,
+    @Body('refreshToken') refreshToken: string,
     @Res({ passthrough: true }) res: Response,
-  ): Promise<WebResponse<RefreshTokenResponse>> {
-    const refreshToken = req.cookies['refresh_token'];
-
-    if (!refreshToken) {
-      throw new HttpException('Refresh token is required', 400);
-    }
+  ): Promise<any> {
     try {
-      const newToken = await this.authService.refreshToken(refreshToken);
-
-      res.cookie('access_token', newToken, {
+      const newAccessToken = await this.authService.refreshToken(refreshToken);
+      res.cookie('access_token', newAccessToken, {
         httpOnly: true,
         secure: false,
         sameSite: 'none',
-        maxAge: 60 * 60 * 1000,
+        maxAge: 30 * 60 * 1000,
       });
       return {
         message: 'New access token created',
       };
-    } catch (error) {
-      console.error('Error during token refresh:', error);
-      throw error;
+    } catch (err: any) {
+      return { error: err.message };
     }
   }
 }
